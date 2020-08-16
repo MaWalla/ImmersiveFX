780a6770c1ab578a9362451bd8808e662bebe149
Max Walaschewski <mawalla@protonmail.com>
2020-08-16 20:54:15 +0200

    rewritten the pulseviz formular for more action

--- pulse.py
+++ pulse.py
@@ -14,13 +14,44 @@ class PulseViz:
         self.kelvin = kelvin
 
         self.band_mapping = {
-            step: [
-                np.clip(255 * math.cos(3.1415926535 / steps * step), 0, 255),
-                255 * math.sin(3.1415926535 / steps * step),
-                np.clip(255 * math.cos(3.1415926535 / steps * step) * -1, 0, 255),
-            ]
-            for step in range(steps)
+            0: [255, 0, 0],
+            1: [255, 0, 0],
+            2: [255, 0, 0],
+            3: [255, 0, 0],
+            4: [255, 0, 0],
+            5: [255, 0, 0],
+            6: [255, 0, 0],
+            7: [255, 0, 0],
+            8: [255, 0, 0],
+            9: [255, 128, 0],
+            10: [255, 255, 0],
+            11: [255, 255, 0],
+            12: [255, 255, 0],
+            13: [255, 255, 0],
+            14: [255, 255, 0],
+            15: [128, 255, 0],
+            16: [0, 255, 0],
+            17: [0, 255, 0],
+            18: [0, 255, 0],
+            19: [0, 255, 0],
+            20: [0, 255, 0],
+            21: [0, 255, 128],
+            22: [0, 255, 255],
+            23: [0, 255, 255],
+            24: [0, 255, 255],
+            25: [0, 255, 255],
+            26: [0, 255, 255],
+            27: [0, 128, 255],
+            28: [0, 0, 255],
+            29: [0, 0, 255],
+            30: [0, 0, 255],
+            31: [0, 0, 255],
+            32: [0, 0, 255],
+            33: [0, 0, 255],
+
         }
+
+
 
         bands_data = {
             'source_name': source_name,
@@ -41,28 +72,33 @@ class PulseViz:
     def stop_bands(self):
         self.pulseviz_bands.stop()
 
+    @staticmethod
+    def data_conversion(value, minimum, maximum):
+        multiplicator = (value - minimum) / (maximum - minimum)
+        return multiplicator * multiplicator * multiplicator
+
+    @staticmethod
+    def post_process_color(value, color):
+        if value != color.max():
+            value *= 0.8
+
+        value *= 255 / (color.max() or 1)
+
+        return value
+
     def loop(self):
         values = self.pulseviz_bands.values
-        flipped_min = values.min() * -1
 
-        converted_values = [
-            [((value + flipped_min) / flipped_min) * color for color in self.band_mapping.get(num)]
-            for num, value in enumerate(values)
+        converted_values = [[
+                self.data_conversion(value, values.min(), values.max()) * color if not np.isinf(value) else 0
+                for color in self.band_mapping.get(num)
+            ] for num, value in enumerate(values)
         ]
 
-        converted_color = np.clip(np.amax(converted_values, axis=0), 0, 255)
-
-        if np.isinf(values.max()):
-            max_color = [0, 0, 0]
-        else:
-            max_color = self.band_mapping.get(np.argmax(values))
-
-        output_color = ((converted_color + np.array(max_color)) / 2.0)
-
-        if np.isnan(output_color.max()):
-            output_color = [0, 0, 0]
-        else:
-            output_color = output_color.astype(int).tolist()
+        color = np.array(converted_values).mean(axis=0)
+        normalized_color = np.clip([
+            self.post_process_color(rgb, color) for rgb in color
+        ], 0, 255).astype(int).tolist()
 
         for nodemcu in self.nodemcus:
             ip = nodemcu.get('ip')
@@ -71,7 +107,7 @@ class PulseViz:
             threading.Thread(
                 target=self.sock.sendto,
                 args=(
-                    bytes(self.prepare_data(output_color), 'utf-8'), (ip, port)
+                    bytes(self.prepare_data(normalized_color), 'utf-8'), (ip, port)
                 ),
                 kwargs={}
             ).start()
